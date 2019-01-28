b207cdb4a357d7e33b4f6c5bbebc499535c28491
YosukeArai13 <yosuke.arai@gmail.com>
2019-01-28 17:48:20 +0900

    0128 commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world: ' + process.env.PODNAME);
+  res.send('Hello Azure world: ' + process.env.PODNAME);
 });
 
 app.listen(PORT);
